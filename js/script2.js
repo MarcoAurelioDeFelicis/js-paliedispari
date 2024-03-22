55234062c67d63609657239a26e3c95bb28443d9
MarcoAurelioDeFelicis <marcodefelicis24@gmail.com>
2024-03-22 20:44:26 +0100

    ADD: script2.js (ragionamento combo split,reverse) ADD: scropt.js (rsgionamento con ciclo for)

--- js/script2.js
+++ js/script2.js
@@ -1,3 +1,6 @@
+console.log ('Palondroma')
+
+
 const parola = prompt ('inserisci una parola')
 console.log (parola)
 
@@ -13,22 +16,17 @@ if (risultato === true ) {
     console.log ('La parola ' + parola + ' non è palindroma')
 }
 
-                     //DICHIARAZIONI FUNZIONI
-                     
 //dichiarazione FUNZIONE
 function isPalindrome (text) {
     console.log(text)//return boolean
 
-    //convertitore in frase minuscole
-    text = text.toLoerCase()
+    //generare stringa invertita
+    const parolaInvertita = parola. toLowerCase(). split(''). reverse(). join('')
+    console.log (parolaInvertita)
 
-    //funzione annidata (generatore di parola invertita)
-    let invertedText = invertiParola(text)
-    
-    console.log(text, invertedText)
-    
+
     //SE parola === parola invertita
-    if (text === invertedText) {
+    if (parola === parolaInvertita) {
         // return true
         return true
         //ALTRIMENTI
@@ -40,18 +38,5 @@ function isPalindrome (text) {
 
 //dichiarazione seconda funzione
 function parolaInvertita (string) {
-    //return stringa inveertita
-
-    //variabile vuota per ciclo FOR
-    let invertedText = ''
-    
-    //generare stringa invertita CICLO FOR
-    for (let i= string.length -1; i >= 0; i--) {
-        //const char = tex.[i]//string or array
-        const char = tex.charAt(i) //string
-        console.log(char)
-        invertedText += char
-    }
-
-    return invertedText
+    //return stringa invertita
 }
